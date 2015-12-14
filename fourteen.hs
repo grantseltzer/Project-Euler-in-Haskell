db8da0bd3fa96ea77fa3db69f788a5caccb109aa
Grant Seltzer Richman <grantseltzer@gmail.com>
2015-12-13 20:16:45 -0500

    Create fourteen.hs

---
+++ fourteen.hs
@@ -0,0 +1,24 @@
+
+-- 14) Which starting number, under one million, produces the longest collatz sequence?
+condition :: Int -> Int
+condition x
+    | x `mod` 2 == 0 = x `div` 2
+    | otherwise = 3*x+1
+
+conList :: Int -> Int
+conList n
+    | n == 1 = []
+    | otherwise = condition(n) : conList(condition(n)))
+
+
+numToList :: [Int] -> [(Int, Int)]
+numToList [] = []
+numToList (x:xs) = (x, length(x:conList(x))):numToList(xs)
+
+maxTuple :: (Int, Int) -> (Int, Int) -> (Int, Int)
+maxTuple (a,b) (c,d)
+    | b > d = (a,b)
+    | d > b = (c,d)
+    | otherwise = (a,b)
+
+fourteen = (foldl maxTuple (1,1) (numToList [1..1000000]))
